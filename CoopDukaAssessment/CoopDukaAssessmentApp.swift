751c4598e08ac4af78ba68c440451dc77dbd05fd
Julius Nyule <jlsnyule@yahoo.com>
2026-03-23 16:20:52 +0300

    Fix a bug and revert isLogged to false on app launch

--- CoopDukaAssessment/CoopDukaAssessmentApp.swift
+++ CoopDukaAssessment/CoopDukaAssessmentApp.swift
@@ -7,53 +7,24 @@
 
 import SwiftUI
 
-//@main
-//struct CoopDukaAssessmentApp: App {
-//    @AppStorage("isLoggedIn") var isLoggedIn: Bool = true
-//    let remoteLoader: RemoteProductLoader
-//    
-//    init() {
-//        let remoteURL = URL(string: "https://jsonplaceholder.typicode.com/posts")!
-//        let client = URLSessionHTTPClient()
-//        self.remoteLoader = RemoteProductLoader(url: remoteURL, client: client)
-//    }
-//    
-//    var body: some Scene {
-//        WindowGroup {
-//            
-//            if isLoggedIn {
-//                NavigationStack {
-//                    ProductsView(viewModel: ProductViewModel(loader: remoteLoader))
-//                }
-//            } else {
-//                LoginView()
-//            }
-//        }
-//    }
-//}
-
 @main
 struct CoopDukaAssessmentApp: App {
-    @AppStorage("isLoggedIn") var isLoggedIn: Bool = false
+    @AppStorage("isLoggedIn") var isLoggedIn: Bool = true
     
-    // Dependencies
     private let remoteLoader: RemoteProductLoader
     private let viewModel: ProductViewModel
     
     init() {
-        // Initialize dependencies properly before 'self' is used
         let url = URL(string: "https://jsonplaceholder.typicode.com")!
         let client = URLSessionHTTPClient()
         let loader = RemoteProductLoader(url: url, client: client)
         
         self.remoteLoader = loader
-        // Pre-initialize the ViewModel so it's ready for the view switch
         self.viewModel = ProductViewModel(loader: loader)
     }
     
     var body: some Scene {
         WindowGroup {
-            // Switch between views based on state
             if isLoggedIn {
                 NavigationStack {
                     ProductsView(viewModel: viewModel)
