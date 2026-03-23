18538b3dc25a0f75c2a0d0120fca25da9d501f76
Julius Nyule <jlsnyule@yahoo.com>
2026-03-23 14:53:00 +0300

    Add api infra

---
+++ CoopDukaAssessment/API Infrastructure/URLSessionHTTPClient.swift
@@ -0,0 +1,40 @@
+//
+//  URLSessionHTTPClient.swift
+//  CoopDukaAssessment
+//
+//  Created by Julius on 23/03/2026.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    struct UnexpectedValuesRepresentation: Error {}
+    
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}
